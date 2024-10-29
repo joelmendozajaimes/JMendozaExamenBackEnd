7cd42bd5f210960b876f7b73a37677b60f331358
joelmendozajaimes <mendozajaimesjoelmagdiel@gmail.com>
2024-10-29 17:55:42 -0600

    script

--- script.sql
+++ script.sql
@@ -1,31 +1,38 @@
 CREATE DATABASE JMendozaExamenBackEnd;
+GO
 
 USE JMendozaExamenBackEnd;
+GO
 
 CREATE TABLE TipoPersonaFiscal(
 	IdTipoPersonaFiscal INT PRIMARY KEY IDENTITY(1,1),
 	Descripcion VARCHAR(50) UNIQUE
 );
+GO
 
 CREATE TABLE Pais(
 	IdPais INT PRIMARY KEY IDENTITY(1,1),
 	Nombre VARCHAR(50) UNIQUE
 );
+GO
 
 CREATE TABLE Ocupacion(
 	IdOcupacion INT PRIMARY KEY IDENTITY(1,1),
 	Nombre VARCHAR(50) UNIQUE
 );
+GO
 
 CREATE TABLE Sexo(
 	IdSexo INT PRIMARY KEY IDENTITY(1,1),
 	Nombre VARCHAR(50) UNIQUE
 );
+GO
 
 CREATE TABLE Banco(
 	IdBanco INT PRIMARY KEY IDENTITY(1,1),
 	Nombre VARCHAR(50) UNIQUE
 );
+GO
 
 CREATE TABLE Persona(
 	IdPersona INT PRIMARY KEY IDENTITY(1,1),
@@ -40,6 +47,7 @@ CREATE TABLE Persona(
 	IdSexo INT REFERENCES Sexo(IdSexo),
 	Estatus BIT
 );
+GO
 
 CREATE TABLE Cuenta(
 	IdCuenta INT PRIMARY KEY IDENTITY(1,1),
@@ -47,30 +55,35 @@ CREATE TABLE Cuenta(
 	IdPersona INT REFERENCES Persona(IdPersona),
 	IdBanco INT REFERENCES Banco(IdBanco)
 );
+GO
 
 INSERT INTO TipoPersonaFiscal(Descripcion)
 VALUES
 ('Física'),
 ('Moral'),
 ('Física con Actividad Empresarial');
+GO
 
 INSERT INTO Pais(Nombre)
 VALUES
 ('México'),
 ('USA'),
 ('Canadá');
+GO
 
 INSERT INTO Ocupacion(Nombre)
 VALUES
 ('Empresario'),
 ('Empleado'),
 ('Comerciante');
+GO
 
 INSERT INTO Sexo(Nombre)
 VALUES
 ('Femenino'),
 ('Masculino'),
 ('No Binario');
+GO
 
 INSERT INTO Banco (Nombre)
 VALUES
@@ -79,7 +92,9 @@ VALUES
 ('Banco Azteca'),
 ('Santander'),
 ('BanCoppel');
+GO
 
 INSERT INTO Persona(Nombre, ApellidoPaterno, ApellidoMaterno, IdTipoPersonaFiscal, RFC, CURP, IdPais, IdOcupacion, IdSexo, Estatus)
 VALUES
 ('Joel Magdiel','Mendoza','Jaimes',1,'MEJJ010420N98','MEJJ010420HDFNMLA7',1,2,2,1);
+GO
